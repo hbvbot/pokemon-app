9164c7e323808bc0b73acd08b0033dc6e1a120aa
Brendan Viloria <bjviloria104@gmail.com>
2019-03-24 19:14:09 -0700

    Add some styling/responsive design

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -20,20 +20,34 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <div className='row'>
-          {this.state.pokemon.map(pokemon => {
-            return (
-              <div className='col-2'>
-                <div>
-                  <img className='img-fluid' src={pokemon.image} />
+        <div className='container-fluid main-container'>
+          <div className='row'>
+            <div className='col-12 pokemon-app-title text-center'>
+              Pokemon App
+            </div>
+          </div>
+          <div className='row'>
+            {this.state.pokemon.map(pokemon => {
+              return (
+                <div className='col-lg-3 col-md-4 col-6 my-5 text-center'>
+                  <div className='pokemon-img-container'>
+                    <img className='pokemon-img' src={pokemon.image} />
+                  </div>
+                  <div>
+                    {pokemon.name} - {pokemon.number}
+                  </div>
+                  <div>
+                    Type:{' '}
+                    {pokemon.types.length > 1
+                      ? pokemon.types.map((type, i) => {
+                          return i === 0 ? `${type} / ` : type;
+                        })
+                      : pokemon.types[0]}
+                  </div>
                 </div>
-                <div>
-                  {pokemon.name} - {pokemon.number}
-                </div>
-                <div>Type: {JSON.stringify(pokemon.types)}</div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     );

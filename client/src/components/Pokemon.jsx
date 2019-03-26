dec37e9942e17b53bd53f7c2e10fdbef719ee28a
Brendan Viloria <bjviloria104@gmail.com>
2019-03-25 21:42:14 -0700

    Style Pokemon info text box

--- client/src/components/Pokemon.jsx
+++ client/src/components/Pokemon.jsx
@@ -11,20 +11,22 @@ class Pokemon extends React.Component {
         <div className='pokemon-img-container d-flex align-content-center justify-content-center'>
           <img className='pokemon-img' src={this.props.image} />
         </div>
-        <div className='mt-2'>
-          {this.props.name} - {this.props.number}
-        </div>
-        <div>
-          Type:{' '}
-          {this.props.types.length > 1
-            ? this.props.types.map((type, i) => {
-                return i === 0 ? `${type} / ` : type;
-              })
-            : this.props.types[0]}
-        </div>
-        <div>ID: {this.props.id}</div>
-        <div>
-          Max CP: {this.props.maxCP} / Max HP: {this.props.maxHP}
+        <div className='py-3 mt-3 pokemon-information'>
+          <div>
+            {this.props.name} - {this.props.number}
+          </div>
+          <div>
+            Type:{' '}
+            {this.props.types.length > 1
+              ? this.props.types.map((type, i) => {
+                  return i === 0 ? `${type} / ` : type;
+                })
+              : this.props.types[0]}
+          </div>
+          <div>ID: {this.props.id}</div>
+          <div>
+            Max CP: {this.props.maxCP} / Max HP: {this.props.maxHP}
+          </div>
         </div>
       </div>
     );
